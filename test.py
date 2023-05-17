ff8f35a42eb7b6e48909a6ad7ed37a0b92e3abc3
Owen <oanyakie@gmail.com>
2023-05-17 16:24:49 +0100

    change test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from binance.client import Client
 client = Client(api_key="1ZdmnuIhGUwmD43exIyW8zrPQkk9lULLDY1zX1jpDYoWTXRnBbxurKs6chlMCCLg" ,api_secret="HtoTg6vCbBy5MqXJFygIYoICD4pRjRzIvuBJnwaprMkTEDkCQELozVVeCajtWUjE")
-status = client.get_account_status()
+status = client.get_account_api_permissions()
 print(status)
